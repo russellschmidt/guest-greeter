e3c16f1723e322bf15bb62e72596e8e30f339718
Russell Schmidt <russell.schmidt@me.com>
2018-08-20 11:58:13 -0700

    add success end session

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ function handleNextQuoteIntent(request, context, session) {
   } else {
     options.speechText = " Wrong invocation of this intent. ";
     options.endSession = true;
+    context.succeed(buildResponse(options));
   }
     
 
