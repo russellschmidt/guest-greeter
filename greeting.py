1bff89f55e99be06043a8bf2519a8505475c5c1b
Russell Schmidt <russell.schmidt@me.com>
2018-08-23 13:25:26 -0700

    add paren to print

--- greeting.py
+++ greeting.py
@@ -92,5 +92,5 @@ class Response(object):
 if __name__ == "__main__":
 	#app.run()
 	port = int(os.getenv('PORT', 5000))
-	print "Starting app on port %d" % port
+	print("Starting app on port %d" % port)
 	app.run(debug=False, port=port, host='0.0.0.0')
